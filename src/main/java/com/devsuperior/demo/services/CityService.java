3710496dfbc6aee9dc117b4544c9b92d42c1bcb3
Leandro Carrijo <7leandrobatista@gmail.com>
2024-01-05 14:33:40 -0300

    feat: created ControllerExceptionHandler and fix DatabaseException

--- src/main/java/com/devsuperior/demo/services/CityService.java
+++ src/main/java/com/devsuperior/demo/services/CityService.java
@@ -3,7 +3,7 @@ package com.devsuperior.demo.services;
 import com.devsuperior.demo.dto.CityDTO;
 import com.devsuperior.demo.entities.City;
 import com.devsuperior.demo.repositories.CityRepository;
-import com.devsuperior.demo.services.exceptions.DatabaseExceptionException;
+import com.devsuperior.demo.services.exceptions.DatabaseException;
 import com.devsuperior.demo.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -40,7 +40,7 @@ public class CityService {
             repository.deleteById(id);
         }
         catch (DataIntegrityViolationException e) {
-            throw new DatabaseExceptionException("Referential integrity failure");
+            throw new DatabaseException("Referential integrity failure");
         }
     }
 }
--- src/main/java/com/devsuperior/demo/services/exceptions/DatabaseExceptionException.java
+++
@@ -1,8 +0,0 @@
-package com.devsuperior.demo.services.exceptions;
-
-public class DatabaseExceptionException extends RuntimeException{
-
-    public DatabaseExceptionException(String msg) {
-        super(msg);
-    }
-}
