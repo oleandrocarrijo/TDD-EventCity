ae992815fd1820f21cb57d56eb7dc39fcc3137c9
Leandro Carrijo <7leandrobatista@gmail.com>
2024-01-05 14:53:32 -0300

    feat: Update Event

--- src/main/java/com/devsuperior/demo/controllers/EventController.java
+++ src/main/java/com/devsuperior/demo/controllers/EventController.java
@@ -1,8 +1,13 @@
 package com.devsuperior.demo.controllers;
 
+import com.devsuperior.demo.dto.EventDTO;
 import com.devsuperior.demo.services.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,4 +16,10 @@ public class EventController {
 
     @Autowired
     private EventService service;
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<EventDTO> update(@PathVariable Long id, @RequestBody EventDTO dto) {
+        dto = service.update(id, dto);
+        return ResponseEntity.ok(dto);
+    }
 }
