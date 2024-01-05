dc3d8df97eff527a90521b896e6573bab85ea855
Leandro Carrijo <7leandrobatista@gmail.com>
2024-01-05 13:14:35 -0300

    feat: created ResourceNotFoundException and DatabaseExceptionException

---
+++ src/main/java/com/devsuperior/demo/services/exceptions/DatabaseExceptionException.java
@@ -0,0 +1,8 @@
+package com.devsuperior.demo.services.exceptions;
+
+public class DatabaseExceptionException extends RuntimeException{
+
+    public DatabaseExceptionException(String msg) {
+        super(msg);
+    }
+}
